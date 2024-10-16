18cd0506f027898f9f376b5e3ddb7f067a751d77
Rogier Spieker <rogier@konfirm.io>
2024-10-16 09:40:43 +0200

    gently start processing

--- chore/mongo-collect.ts
+++ chore/mongo-collect.ts
@@ -1,3 +1,20 @@
-const { MONGO_VERSION = 'latest' } = process.env;
+import { resolve } from 'node:path';
+import { Version } from '../source/domain/version';
+import { readJSONFile } from '../source/domain/json';
 
-console.log({ MONGO_VERSION });
+const { MONGO_VERSION = '8' } = process.env;
+const version = new Version(MONGO_VERSION);
+const automation = resolve(__dirname, '..', 'automation');
+const catalogFile = resolve(automation, 'catalog-queries.json');
+const metaFile = resolve(automation, 'collect', `v${version.major}`, String(version), 'meta.json');
+
+Promise.resolve()
+    .then(() => {
+        console.log({ version, string: String(version), number: Number(version) });
+    })
+    .then(() => readJSONFile(metaFile))
+    .then(async (meta: any) => {
+        const catalog = await readJSONFile(catalogFile);
+
+        console.log({ meta, catalog });
+    });
