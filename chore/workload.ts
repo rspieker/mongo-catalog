67db8e3192640b2f87ec1bd3ccb98367dd577e3b
Rogier Spieker <rogier@konfirm.io>
2024-10-16 10:24:06 +0200

    limit the workload to a maximum of the five highest versions per major version
    maybe run more often

--- chore/workload.ts
+++ chore/workload.ts
@@ -1,6 +1,7 @@
-import { resolve } from "node:path";
-import { glob } from "glob";
-import { readJSONFile } from "../source/domain/json";
+import { resolve } from 'node:path';
+import { glob } from 'glob';
+import { readJSONFile } from '../source/domain/json';
+import { Version } from '../source/domain/version';
 
 const automation = resolve(__dirname, '..', 'automation');
 const catalogFile = resolve(automation, 'catalog-queries.json');
@@ -23,6 +24,19 @@ readJSONFile<Array<any>>(catalogFile)
         }
 
         return [...outdated];
+    })
+    .then((outdated: Array<string>) => {
+        const grouped = outdated
+            .map((version: string) => new Version(version))
+            .reduce((carry, version) => {
+                carry[version.major] = (carry[version.major] || []).concat(version);
+
+                return carry;
+            }, {} as { [key: string]: Array<Version> });
+
+        return Object.keys(grouped)
+            .map((key) => grouped[String(key)].sort((a: Version, b: Version) => a > b ? -1 : Number(a < b)))
+            .flatMap((prioritized) => prioritized.slice(0, 5).map(String));
     })
     .then((outdated: Array<string>) => {
         console.log(JSON.stringify(outdated));
